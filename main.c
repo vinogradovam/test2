cfe742712a6388e469983f656764b46115c597da
Ivanov <ivanov@m.ru>
2015-05-04 23:49:56 +0300

    test revert

--- main.c
+++ main.c
@@ -10,5 +10,7 @@ int main(int argc, char **argv)
     askname(first, last);
 
     printf("Hello, %s %s!\n", first, last);
+//---------------------------------
     return 0;
+//---------------------------------
 }
