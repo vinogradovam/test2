cf005c0a746c3c8e667355bac811c68cc981c566
vinogradovam <vinogradova.m@yandex.ru>
2015-05-05 17:34:14 +0300

    remote edit

--- main.c
+++ main.c
@@ -6,9 +6,9 @@
 int main(int argc, char **argv)
 {
     char first[255], last[255];
-
+//---------------------------------
     askname(first, last);
-
+//---------------------------------
     printf("Hello, %s %s!\n", first, last);
 //---------------------------------
     return 0;
